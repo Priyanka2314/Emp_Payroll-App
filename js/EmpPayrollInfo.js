e24f20754bcc61617819a6fd55fa990d7ae14642
Priyanka <priyankacs102@gmail.com>
2021-01-02 14:03:37 +0530

    UC11 -Add new attributes Profile,Gender,Department & Note

--- js/EmpPayrollInfo.js
+++ js/EmpPayrollInfo.js
@@ -13,33 +13,7 @@ class EmployeePayroll{
       this._name =name;
     else
       throw "Name is InCorrect";
-  }
-  
-  get profilePic(){ return this._profilePic; }
-  set profilePic(profilePic){
-    this._profilePic=profilePic;
-  }
-  
-  get gender() { return this._gender; }
-  set gender(gender) {
-    this._gender = gender;
-  }
-  
-  get department() { return this._department; }
-  set department(department) {
-      this._department = department;
-  }
-  
-  get salary() { return this._salary; }
-  set salary(salary) {
-    this._salary = salary;
-  }
-  
-  get note() { return this._note; }
-  set note(note) {
-  this._note = note;
-  }
-  
+  } 
   get startDate() { return this._startDate; }
   set startDate(startDate) {
     //UC10 - date func will provide todays date
@@ -62,5 +36,31 @@ class EmployeePayroll{
   const empDate = !this.startDate ? "undefined" :this.startDate;
   return "id = "+this.id+ " ,name = "+this.name+" , gender ="+this.gender+" , profilePic ="+this.profilePic+" , department ="+this.department+
           " ,salary = "+ this.salary+ " ,startDate = "+empDate+ ",note =" +this.note;
+  }
+
+/*UC 11 */
+get profilePic(){ return this._profilePic; }
+  set profilePic(profilePic){
+    this._profilePic=profilePic;
+  }
+  
+  get gender() { return this._gender; }
+  set gender(gender) {
+    this._gender = gender;
+  }
+  
+  get department() { return this._department; }
+  set department(department) {
+      this._department = department;
+  }
+  
+  get salary() { return this._salary; }
+  set salary(salary) {
+    this._salary = salary;
+  }
+  
+  get note() { return this._note; }
+  set note(note) {
+  this._note = note;
   }
 }
