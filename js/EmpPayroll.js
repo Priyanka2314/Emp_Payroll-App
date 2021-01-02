0ead17824f92db517b91d2ae6082e01d1ad0116a
Priyanka <priyankacs102@gmail.com>
2021-01-02 11:36:02 +0530

    UC8: Add even listener on salary-range

---
+++ js/EmpPayroll.js
@@ -0,0 +1,7 @@
+// UC8 -Add eventListener on salary range
+const salary= document.querySelector('#salary');
+const output = document.querySelector('.salary-output');
+output.textContent = salary.value;
+salary.addEventListener('input',function(){
+output.textContent =salary.value;
+});
