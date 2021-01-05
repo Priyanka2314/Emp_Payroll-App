34e38a4cf7c691a7bc1536413affed5b5f972901
Priyanka <priyankacs102@gmail.com>
2021-01-06 00:33:45 +0530

    Attaching an event handler for the load of DOM content

--- js/EmpPayroll.js
+++ js/EmpPayroll.js
@@ -1,3 +1,32 @@
+//UC10 - Valdation for Name and Date
+//UC12 
+/**
+ * To be executed after loading the DOM content- Document Object Model i.e. the webpage
+ *  Attaching an event handler for the load of DOM content 
+ */
+window.addEventListener('DOMContentLoaded',(event) =>{
+//validation for name
+const name =document.querySelector('#name');
+const textError = document.querySelector('.text-error');
+ // Add event handler to the name field of the main page
+name.addEventListener('input', function(){
+  // If name is empty i.e. user did not entered any value no error message
+  if(name.value.length ===0){
+    textError.textContent ="";
+    return;
+  }
+  /**
+   * else validate the name and then print the specified error message to the screen
+   * Error-message specified in the event by internally validating to the setter in EmployeePayroll Class
+   */
+  try{
+    (new EmployeePayroll()).name = name.value;
+    textError.textContent ="";
+  }catch(e){
+    textError.textContent = e;
+  }
+});
+
 // UC8 -Add eventListener on salary range
 const salary= document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
@@ -6,23 +35,6 @@ salary.addEventListener('input',function(){
 output.textContent =salary.value;
 });
 
-//UC10 - Valdation for Name and Date
-window.addEventListener('DOMContentLoaded',(event) =>{
-//validation for name
-const name =document.querySelector('#name');
-const textError = document.querySelector('.text-error')
-name.addEventListener('input', function(){
-  if(name.value.length ===0){
-    textError.textContent ="";
-    return;
-  }
-  try{
-    (new EmployeePayroll()).name = name.value;
-    textError.textContent ="";
-  }catch(e){
-    textError.textContent = e;
-  }
-});
 //Validation for date
 const date = document.querySelector('#date');
 const dateError = document.querySelector('.date-error');
@@ -41,4 +53,7 @@ getInputValueById('#year');
     }
 
   });
-})
+});
+
+
+
