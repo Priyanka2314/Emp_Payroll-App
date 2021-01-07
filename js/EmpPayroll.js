025cb29242d125b90885a9ca423cf68e05d73409
Priyanka <priyankacs102@gmail.com>
2021-01-07 06:01:47 +0530

    Adding: UC13 Employee Payroll object on save

--- js/EmpPayroll.js
+++ js/EmpPayroll.js
@@ -1,44 +1,116 @@
-// UC8 -Add eventListener on salary range
-const salary= document.querySelector('#salary');
-const output = document.querySelector('.salary-output');
-output.textContent = salary.value;
-salary.addEventListener('input',function(){
-output.textContent =salary.value;
-});
-
 //UC10 - Valdation for Name and Date
+//UC12 
+/**
+ * To be executed after loading the DOM content- Document Object Model i.e. the webpage
+ *  Attaching an event handler for the load of DOM content 
+ */
 window.addEventListener('DOMContentLoaded',(event) =>{
-//validation for name
-const name =document.querySelector('#name');
-const textError = document.querySelector('.text-error')
-name.addEventListener('input', function(){
-  if(name.value.length ===0){
-    textError.textContent ="";
-    return;
-  }
-  try{
-    (new EmployeePayroll()).name = name.value;
-    textError.textContent ="";
-  }catch(e){
-    textError.textContent = e;
-  }
-});
-//Validation for date
-const date = document.querySelector('#date');
-const dateError = document.querySelector('.date-error');
-const getInputValueById = (id) => {
-let value = document.querySelector(id).value;
-  return value;
-}
-date.addEventListener('click', function () {
-let startDate = getInputValueById('#day') + " " + getInputValueById('#month') + " " +
-getInputValueById('#year');
-  try {
-    (new EmployeePayRoll()).startDate = new Date(Date.parse(startDate));
-    dateError.textContent = "";
-  } catch (e) {
-      dateError.textContent = e;
+  //validation for name
+  const name =document.querySelector('#name');
+  const textError = document.querySelector('.text-error');
+   // Add event handler to the name field of the main page
+  name.addEventListener('input', function(){
+    // If name is empty i.e. user did not entered any value no error message
+    if(name.value.length ===0){
+      textError.textContent ="";
+      return;
     }
-
+    /**
+     * else validate the name and then print the specified error message to the screen
+     * Error-message specified in the event by internally validating to the setter in EmployeePayroll Class
+     */
+    try{
+      (new EmployeePayroll()).name = name.value;
+      textError.textContent ="";
+    }catch(e){
+      textError.textContent = e;
+    }
   });
-})
+  
+  // UC8 -Add eventListener on salary range
+  const salary= document.querySelector('#salary');
+  const output = document.querySelector('.salary-output');
+  output.textContent = salary.value;
+  salary.addEventListener('input',function(){
+  output.textContent =salary.value;
+  });
+  
+  //Validation for date
+  const date = document.querySelector('#date');
+  const dateError = document.querySelector('.date-error');
+  const getInputValueById = (id) => {
+  let value = document.querySelector(id).value;
+    return value;
+  }
+  date.addEventListener('input', function () {
+  let startDate = getInputValueById('#day') + " " + getInputValueById('#month') + " " +
+  getInputValueById('#year');
+    try {
+      (new EmployeePayRoll()).startDate = new Date(Date.parse(startDate));
+      dateError.textContent = "";
+    } catch (e) {
+        dateError.textContent = e;
+      }
+  
+    });
+  });
+  //UC13 - Create Employee Payroll object on save
+  const save = () => {
+    try{
+      let employeePayRollData = createEmployeePayroll();
+    }catch(e){
+      return;
+    }
+  }
+
+  const createEmployeePayroll = () =>{
+    let employeePayRollData = new EmployeePayroll();
+    try{
+      employeePayRollData.name = getInputValueById('#name');
+    }catch(e){
+      setTextValue('.text-error',e);
+      throw e;
+    }
+    employeePayRollData.profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayRollData.gender = getSelectedValues('[name=gender]').pop();
+    employeePayRollData.department = getSelectedValues('[name=department]');
+    employeePayRollData.salary = getInputValueById('#salary');
+    employeePayRollData.note =getInputValueById('#notes');
+    let date = getInputValueById('#day')+" "+getInputValueById('#month')+" "+ getInputValueById('#year');
+    employeePayRollData.date =Date.parse(date);
+    alert(employeePayRollData.toString());
+    return employeePayRollData;
+  }
+
+  const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let setItems = [];
+    allItems.forEach(item => {
+      if(item.checked) 
+       setItems.push(item.value);
+    });
+    return setItems;
+  }
+  
+  /**
+   * 1: queryselector is the newer feature.
+   * 2: The queyselector method can be used when selecting by element name, nesting, or class name.
+   * 3: querySelector lets you find elements with rules that can't be expressed with getElementById
+   */
+  
+   const getInputValueById =(id) =>{
+     let value = document.querySelector(id).value;
+     return value;
+   }
+  
+   /**
+    * 1: getElementById is better supported than querySelector in older versions
+    *    of the browsers.
+    * 2: The thing with getElementById is that it only allows to select an element by its id.
+    */
+  
+    const getInputElementValue = (id) =>{
+      let value = document.getElementById(id).value;
+      return value;
+    }
+  
