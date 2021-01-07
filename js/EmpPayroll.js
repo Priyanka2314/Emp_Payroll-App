44eba61971e00d70996469aca5a3de19e500b2fe
Priyanka <priyankacs102@gmail.com>
2021-01-07 07:25:10 +0530

    Add: UC15  reset the form

--- js/EmpPayroll.js
+++ js/EmpPayroll.js
@@ -131,6 +131,37 @@ const getSelectedValues = (propertyValue) => {
     return value;
   }
 
+  //Instead of reset method we can call form button reset
+
+  const restForm =() =>{
+    setValue('#name','');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+    setValue('#salary','');
+    setValue('#notes','');
+    setValue('#day','1');
+    setValue('#month','January');
+    setValue('#year','2020');
+  }
+
+  const unsetSelectedValues = (propertyValue) =>{
+    let allItems =document.querySelectorAll(propertyValue);
+    allItems.forEach(item => {
+      item.checked = false;
+    });
+  }
+
+  const setTextValue =(id,value) => {
+    const element = document.querySelector(id);
+    element.textContent =value;
+  }
+
+  const setValue = (id,value) => {
+    const element = document.querySelector(id);
+    element.value =value ;
+  }
+
 
 
 
